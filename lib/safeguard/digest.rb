8714aa04a259a531a4df9ae286b01a26790ef72e
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-14 21:45:13 -0200

    Document Digest module.

--- lib/safeguard/digest.rb
+++ lib/safeguard/digest.rb
@@ -1,8 +1,11 @@
 require 'openssl'
 
 module Safeguard
+
+  # Encapsulates checksum computation for files using a set of hash functions.
   module Digest
 
+    # Compute the SHA1 sum of a file.
     def self.file(filename)
       OpenSSL::Digest::SHA1.file(filename).hexdigest
     end
