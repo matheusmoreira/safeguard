18db9a038323192333b87090acb07a6862a453ad
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-14 19:41:40 -0200

    Add a HashTable.

---
+++ lib/safeguard/hash_table.rb
@@ -0,0 +1,41 @@
+require 'safeguard/digest'
+require 'yaml'
+
+module Safeguard
+
+  # Holds filename => checksum pairs.
+  class HashTable
+
+    # Initializes an empty +HashTable+.
+    def initialize
+      @table = {}
+    end
+
+    # Saves the +HashTable+ to a YAML file.
+    def save(filename)
+      File.open(filename, 'w') do |file|
+        file.puts to_yaml
+      end
+    end
+
+    # Loads the +HashTable+ from a YAML file.
+    def self.load(filename)
+      YAML::load_file filename
+    end
+
+    # Associates the given +filename+ to the computed checksum of the file it
+    # refers to.
+    def <<(filename)
+      @table[filename] = Digest.file filename
+    end
+
+    alias :add :<<
+
+    # Looks up the checksum for the given +filename+.
+    def [](filename)
+      @table[filename]
+    end
+
+  end
+
+end
