7851dde6d317b328a5d89263c26a40d08f348247
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-14 21:45:23 -0200

    Document Version module.

--- lib/safeguard/version.rb
+++ lib/safeguard/version.rb
@@ -1,11 +1,30 @@
 module Safeguard
+
+  # Contains the current version of Safeguard.
   module Version
 
+    # Major version.
+    #
+    # Increments denote backward-incompatible changes and additions.
     MAJOR = 0
+
+    # Minor version.
+    #
+    # Increments denote backward-compatible changes and additions.
     MINOR = 0
+
+    # Patch version.
+    #
+    # Increments denote changes in implementation.
     PATCH = 1
+
+    # Build version.
+    #
+    # Used for pre-release versions.
     BUILD = nil
 
+    # Complete version string, which is every individual version number joined
+    # by a dot ('.'), in descending order of prescedence.
     STRING = [ MAJOR, MINOR, PATCH, BUILD ].compact.join '.'
 
   end
