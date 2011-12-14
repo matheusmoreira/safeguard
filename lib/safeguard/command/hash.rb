95b2d152ae837a5adb15655757dfca94f9000536
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-14 21:44:52 -0200

    Document hash command.

--- lib/safeguard/command/hash.rb
+++ lib/safeguard/command/hash.rb
@@ -3,10 +3,13 @@ require 'safeguard/digest'
 
 module Safeguard
   module Command
+
+    # Outputs the SHA1 hash of files.
     module Hash
 
       Command.register self
 
+      # For every argument, outputs its SHA1 sum if it exists as a file.
       def self.execute(*args)
         args.each do |filename|
           puts "#{Digest.file filename} => #{filename}" if File.file? filename
