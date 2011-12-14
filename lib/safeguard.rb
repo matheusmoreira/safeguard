639d67602d59bd500ac9c41e7c45e27d41f6185b
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-14 21:25:17 -0200

    Document the Safeguard module.

--- lib/safeguard.rb
+++ lib/safeguard.rb
@@ -1,7 +1,9 @@
 require 'safeguard/command'
 
+# Safeguard module.
 module Safeguard
 
+  # Run a command by name with the given arguments.
   def self.run(command, *args)
     Command.invoke(command, *args)
   end
