e5358706b5178f6333c13239959588f92f273b70
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-14 20:24:12 -0200

    Load HashTable from repository or create a new one.

--- lib/safeguard/repository.rb
+++ lib/safeguard/repository.rb
@@ -1,7 +1,12 @@
+require 'safeguard/hash_table'
+
 module Safeguard
+
   class Repository
 
     DIRECTORY_NAME = '.safeguard'.freeze
+
+    HASH_TABLE_FILE_NAME = 'hash_table'.freeze
 
     attr_accessor :directory
     alias :dir :directory
@@ -9,6 +14,14 @@ module Safeguard
     def initialize(dir)
       self.directory = Repository.directory_in(dir)
       FileUtils.mkdir_p directory
+    end
+
+    def hash_table
+      HashTable.load hash_table_file_name rescue HashTable.new
+    end
+
+    def hash_table_file_name
+      File.join(directory, HASH_TABLE_FILE_NAME)
     end
 
     def self.directory_in(dir)
