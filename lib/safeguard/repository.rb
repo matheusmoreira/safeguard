4614470baca10430458ccbb62c70802e2a8c95db
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-14 18:23:26 -0200

    Add repository class.

---
+++ lib/safeguard/repository.rb
@@ -0,0 +1,22 @@
+module Safeguard
+  class Repository
+
+    DIRECTORY_NAME = '.safeguard'.freeze
+
+    attr_accessor :directory
+
+    def initialize(dir)
+      self.directory = Repository.directory_in(dir)
+      FileUtils.mkdir_p directory
+    end
+
+    def self.directory_in(dir)
+      File.join(dir, DIRECTORY_NAME)
+    end
+
+    def self.initialized?(dir)
+      File.directory? directory_in(dir)
+    end
+
+  end
+end
