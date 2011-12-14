42de27c607751c2914bcc58edb9b343000429415
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-14 20:58:53 -0200

    Document init command module.

--- lib/safeguard/command/init.rb
+++ lib/safeguard/command/init.rb
@@ -4,10 +4,15 @@ require 'fileutils'
 
 module Safeguard
   module Command
+
+    # Initializes a Repository in a given directory.
     module Init
 
       Command.register self
 
+      # Initializes a Safeguard Repository in a directory, which is either the
+      # current directory, if +args+ is empty, or the directory designated by
+      # the last element in +args+.
       def self.execute(*args)
         args << Dir.pwd if args.empty?
         dir = File.expand_path args.pop
