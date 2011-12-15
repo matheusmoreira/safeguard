96a4bf29315dea87650b39a1b47bb41fe48d6d86
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-15 20:22:01 -0200

    Add Digest::digest_file private module method.

--- lib/safeguard/digest.rb
+++ lib/safeguard/digest.rb
@@ -5,9 +5,21 @@ module Safeguard
   # Encapsulates checksum computation for files using a set of hash functions.
   module Digest
 
+    # Digests a file using an +algorithm+.
+    #
+    # Algorithms are expected include the Digest::Instance module.
+    #
+    #   OpenSSL::Digest::SHA1.include? Digest::Instance
+    #   => true
+    def self.digest_file(filename, algorithm)
+      algorithm.file(filename).hexdigest
+    end
+
+    private_class_method :digest_file
+
     # Compute the SHA1 sum of a file.
     def self.sha1(filename)
-      OpenSSL::Digest::SHA1.file(filename).hexdigest
+      digest_file filename, OpenSSL::Digest::SHA1
     end
 
   end
