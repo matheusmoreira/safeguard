9b6e0a84f2325a55f8376d22ab89421a62b7ced3
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-18 05:53:52 -0200

    Add option for the hash function to the hash command.

--- lib/safeguard/command/hash.rb
+++ lib/safeguard/command/hash.rb
@@ -4,13 +4,27 @@ require 'safeguard/digest'
 module Safeguard
   class Command
 
-    # Outputs the SHA1 hash of files.
+    # Outputs the checksum of files.
+    #
+    #   $ safeguard hash *.mp3
+    #
+    # The hash function can be specified with the --hash-function option. The
+    # default is SHA1, but MD5 and CRC32 are also supported.
+    #
+    #   $ safeguard --hash-function md5 hash *.mp3
     class Hash < Command
 
-      # For every argument, outputs its SHA1 sum if it exists as a file.
+      opt :func, names: %w(--hash-function --algorithm),
+                 default: :sha1, arity: [1,0],
+                 description: 'Algorithm to use. Currently supported: ' <<
+                              Digest::SUPPORTED_ALGORITHMS.join(', ')
+
+      # For every argument, outputs its checksum if it exists as a file.
       action do |options, args|
         args.each do |filename|
-          puts "#{Digest.file filename} => #{filename}" if File.file? filename
+          next unless File.readable? filename
+          digest = Digest.file filename, options.func
+          puts "#{digest} => #{filename}"
         end
       end
 
