054a28b062a066131ff3b569c28826bd76a5d5be
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2011-12-30 20:08:02 -0200

    Improve hash function option description in Hash command.

--- lib/safeguard/command/hash.rb
+++ lib/safeguard/command/hash.rb
@@ -15,7 +15,8 @@ module Safeguard
     class Hash < Command
 
       opt :func, '--function', '--hash-function', '--algorithm',
-                 "Algorithm to use. Currently supported: #{Digest::SUPPORTED_ALGORITHMS.join(', ')}",
+                 "Algorithm to use to calculate the file's checksum. " <<
+                 "Currently supported: #{Digest::SUPPORTED_ALGORITHMS.join(', ')}",
                  default: :sha1, arity: [1,0]
 
       # For every argument, outputs its checksum if it exists as a file.
