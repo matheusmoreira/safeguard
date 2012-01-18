f7455ad39ca297aa8439b48e9fd8da0764528736
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-01-18 20:11:51 -0200

    Patch add command to simply add the files to the repository.

--- lib/safeguard/command/add.rb
+++ lib/safeguard/command/add.rb
@@ -1,5 +1,4 @@
 require 'safeguard/command'
-require 'safeguard/digest'
 
 module Safeguard
   class Command
@@ -7,19 +6,11 @@ module Safeguard
     # Adds files to a Repository.
     class Add < Command
 
-      opt :functions, '--use', Symbol,
-                      "Algorithm used to calculate the file's checksum. " <<
-                      "Currently supported: #{Digest::SUPPORTED_ALGORITHMS.join(', ')}",
-                      arity: [1,0], on_multiple: :append
-
-      opt :force, '--force', 'Rehash files that are already in the repository'
-
-      # For every argument, try to add it to the Repository in the current
-      # directory.
+      # For every argument, add it to the Repository in the current directory.
       action do |options, files|
         repo = Repository.new options.dir
         repo.before_save do
-          repo.hash_and_add! *files, functions: options.functions, force: options.force?
+          repo.add! *files
         end
       end
 
@@ -27,3 +18,5 @@ module Safeguard
 
   end
 end
+
+require 'safeguard/command/add/hash'
