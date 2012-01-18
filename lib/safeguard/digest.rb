28f073d2cdd5fdc368e85dfb6bf51f682f068013
Matheus Moreira <matheus.a.m.moreira@gmail.com>
2012-01-18 14:09:37 -0200

    Put class methods under class << self.

--- lib/safeguard/digest.rb
+++ lib/safeguard/digest.rb
@@ -20,36 +20,39 @@ module Safeguard
       end
     end
 
-    # Digests a file using an +algorithm+.
-    #
-    # Algorithms are expected include the Digest::Instance module.
-    #
-    #   OpenSSL::Digest::SHA1.include? Digest::Instance
-    #   => true
-    def self.digest_with(algorithm, filename)
-      digest = algorithm.new
-      File.open(filename, 'rb') do |file|
-        file.each_chunk do |chunk|
-          digest << chunk
+    # The class methods.
+    class << self
+
+      # Digests a file using an +algorithm+.
+      #
+      # Algorithms are expected include the Digest::Instance module.
+      #
+      #   OpenSSL::Digest::SHA1.include? Digest::Instance
+      #   => true
+      def digest_with(algorithm, filename)
+        digest = algorithm.new
+        File.open(filename, 'rb') do |file|
+          file.each_chunk do |chunk|
+            digest << chunk
+          end
         end
+        digest.hexdigest!
       end
-      digest.hexdigest!
-    end
 
-    private_class_method :digest_with
+      # Digests a file using a hash function, which can be the symbol of any
+      # Digest module method that takes a file. Uses SHA1 by default.
+      #
+      #   Safeguard::Digest.file(file, :md5)
+      #
+      # Is equivalent to:
+      #
+      #   Safeguard::Digest.md5(file)
+      def file(file, hash_function = :sha1)
+        f = hash_function.to_sym
+        raise ArgumentError, "Unsupported hash function: #{f}" unless respond_to? f
+        send hash_function, file
+      end
 
-    # Digests a file using a hash function, which can be the symbol of any
-    # Digest module method that takes a file. Uses SHA1 by default.
-    #
-    #   Safeguard::Digest.file(file, :md5)
-    #
-    # Is equivalent to:
-    #
-    #   Safeguard::Digest.md5(file)
-    def self.file(file, hash_function = :sha1)
-      f = hash_function.to_sym
-      raise ArgumentError, "Unsupported hash function: #{f}" unless respond_to? f
-      send hash_function, file
     end
 
   end
